91f9797c866230fca087dc38b1a83a5a38bbb2ea
laeretria <laeretria@gmail.com>
2024-03-26 10:57:52 +0100

    initial commit

---
+++ button_receiver/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from time import sleep  # Allows us to call the sleep function to slow down our loop
+import RPi.GPIO as GPIO # Allows us to call our GPIO pins and names it just GPIO
+import pyautogui
+
+keymapping = {
+    "21": "A",
+    "14": "B",
+    "27": "C",
+}
+
+ 
+GPIO.setmode(GPIO.BCM)  # Set's GPIO pins to BCM GPIO numbering
+PINS = [21,14,27]        # Sets our input pin, in this example I'm connecting our button to pin 4. Pin 0 is the SDA pin so I avoid using it for sensors/buttons
+
+for pin in PINS:
+    GPIO.setup(pin, GPIO.IN)
+    GPIO.add_event_detect(pin, GPIO.FALLING, callback=lambda p: callback(p), bouncetime=30)
+
+# Create a function to run when the input is high
+def callback(pin: int):
+    pyautogui.press(keymapping[str(pin)])
+
+
+# Start a loop that never ends
+while True:
+    sleep(1)           # Sleep for a full second before restarting our loop
---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('keypress', function (e) {
+    if (e.key === 'A') {
+        // Handle key A
+    } else if (e.key === 'B') {
+        // Handle key B
+    } else if (e.key === 'C') {
+        // Handle key C
+    } else {
+        console.log('key unrecognized');
+    }
+});
